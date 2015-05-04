a9117ffa38e36d7d61a8ebd56c402e48c3eaf803
Haris Skiadas <cskiadas@gmail.com>
2015-05-04 02:19:39 -0400

    Basic Point implementation

--- app/point.js
+++ app/point.js
@@ -1,6 +1,8 @@
 (function(define) {'use strict';
 define(function(require) {
 
+return function(Graphic) {
+
    var Point, Component, mixin, newClass;
 
    mixin = require('../lib/mixin');
@@ -11,12 +13,10 @@ define(function(require) {
     * Point Properties:
     * - `coords`: The point's coordinates
     * @class Point
-    * @classdesc Represents a graphical Point ("circle")
-    *
+    * @classdesc Represents a "statistical" Point.
+    * A "circle" by default.
     */
-   Point = newClass(function init() {
-
-   }, Component);
+   Point = newClass(function init() {}, Component);
    mixin(Point, {
       defaults: {
          x: 0,
@@ -24,12 +24,28 @@ define(function(require) {
       }
    });
    mixin(Point.prototype, {
+      prepare: function() {
+         this.point = Graphic.Circle.new();
+      },
+      attr: function(o) {
+         mixin(this, o);
+         this.point.set(o);
+         return this;
+      },
       update: function() {
-
+         this.point.update();
+         return this;
+      },
+      parent: function(newParent) {
+         return this.point.parent.apply(this.point, arguments);
+      },
+      getRealization: function() {
+         return this.point.getRealization();
       }
    });
 
    return Point;
+};
 
 });
 
